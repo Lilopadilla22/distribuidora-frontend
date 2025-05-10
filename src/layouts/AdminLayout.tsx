7ea0c2ba7af58a3e7129849dae681dd7a9a0dd61
lilia.padilla <lilia.padilla@beex.ar>
2025-05-10 17:18:09 -0500

    Feature logout in pages

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,5 +1,21 @@
+import { useAuthStore } from '../context/useAuthStore'
+import { useNavigate } from 'react-router-dom'
+
 export const AdminLayout = () => {
+  const logout = useAuthStore((state) => state.logout)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/login', { replace: true })
+  }
+
   return (
-    <div>AdminLayout</div>
+    <div>
+      <header>
+        <h2>Panel de administrador</h2>
+        <button onClick={handleLogout}>Cerrar sesión</button>
+      </header>
+    </div>
   )
 }
--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -1,5 +1,21 @@
+import { useAuthStore } from '../context/useAuthStore'
+import { useNavigate } from 'react-router-dom'
+
 export const ClientLayout = () => {
+  const logout = useAuthStore((state) => state.logout)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/login', { replace: true })
+  }
+
   return (
-    <div>ClientLayout</div>
+    <div>
+      <header>
+        <h2>Bienvenido cliente</h2>
+        <button onClick={handleLogout}>Cerrar sesión</button>
+      </header>
+    </div>
   )
 }
