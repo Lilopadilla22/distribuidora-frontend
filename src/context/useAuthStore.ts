15046e28245ce8ec0b761e559d01c723645d7878
lilia.padilla <lilia.padilla@beex.ar>
2025-05-10 17:17:54 -0500

    add logout

--- src/context/useAuthStore.ts
+++ src/context/useAuthStore.ts
@@ -2,8 +2,6 @@ import { create } from 'zustand'
 import type { User } from './types/user'
 import { persist } from 'zustand/middleware'
 
-
-
 type AuthState = {
   user: User | null
   login: (user: User) => void
@@ -15,7 +13,10 @@ export const useAuthStore = create<AuthState>()(
     (set) => ({
       user: null,
       login: (user) => set({ user }),
-      logout: () => set({ user: null }),
+      logout: () => {
+        set({ user: null })
+        localStorage.removeItem('auth-storage') 
+      },
     }),
     {
       name: 'auth-storage', 
