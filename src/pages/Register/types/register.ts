99948d5f94657c7c54ed375b62d9229c9479236d
lilia.padilla <lilia.padilla@beex.ar>
2025-05-10 16:58:16 -0500

    feature logic in register to user

---
+++ src/pages/Register/types/register.ts
@@ -0,0 +1,11 @@
+export type RegisterFormData = {
+  nombre: string
+  apellido: string
+  email: string
+  password: string
+  direccion: string
+  telefono: string
+  fecha_cumpleanos: string
+  cc: string
+  nombre_negocio: string
+}
