6f9be76c29f977fbfb3c637be469fa1aa75d6cff
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-05-18 15:25:42 -0500

    add types.

---
+++ src/types/products/products.ts
@@ -0,0 +1,19 @@
+export interface Category {
+  _id: string;
+  nombre: string;
+}
+
+export interface Product {
+  _id: string;
+  nombre: string;
+  descripcion: string;
+  categoria: Category;
+  precio_por_kilo: number;
+  precio_por_unidad: number;
+  unidad: string;
+  disponible: boolean;
+  recomendado: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
