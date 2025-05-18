95c8e359620ace46ab4f79101986aac75560f3ab
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-05-18 15:25:23 -0500

    updayte client layout

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -1,21 +1,7 @@
-import { useAuthStore } from '../context/useAuthStore'
-import { useNavigate } from 'react-router-dom'
+import Home from '../pages/Home/Home'
+
 
 export const ClientLayout = () => {
-  const logout = useAuthStore((state) => state.logout)
-  const navigate = useNavigate()
 
-  const handleLogout = () => {
-    logout()
-    navigate('/login', { replace: true })
-  }
-
-  return (
-    <div>
-      <header>
-        <h2>Bienvenido cliente</h2>
-        <button onClick={handleLogout}>Cerrar sesión</button>
-      </header>
-    </div>
-  )
+  return <Home/>
 }
