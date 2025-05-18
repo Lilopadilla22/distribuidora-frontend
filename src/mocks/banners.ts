5fef5f63960c6915319de7dc22d1e6b266b743fb
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-05-18 15:25:31 -0500

    add mocks

---
+++ src/mocks/banners.ts
@@ -0,0 +1,34 @@
+import bannerPromotional1 from '../assets/bannerPromotional5.png';
+import bannerPromotional2 from '../assets/bannerPromotional2.png';
+import bannerPromotional3 from '../assets/bannerPromotional3.png';
+import bannerPromotional from '../assets/bannerPromotional.png';
+
+
+const banners = [
+  {
+    id: 0,
+    image: bannerPromotional,
+    title: "Calidad garantizada",
+    description: "Todos nuestros productos cuentan con sellos de calidad",
+  },
+  {
+    id: 1,
+    image: bannerPromotional1,
+    title: "Productos frescos diariamente",
+    description: "Recibimos productos frescos todos los días",
+  },
+  {
+    id: 2,
+    image: bannerPromotional2,
+    title: "Entregas a domicilio",
+    description: "Hacemos entregas en toda la ciudad",
+  },
+  {
+    id: 3,
+    image: bannerPromotional3,
+    title: "Calidad garantizada",
+    description: "Todos nuestros productos cuentan con sellos de calidad",
+  },
+];
+
+export default banners
