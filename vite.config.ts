e3bf651548bab782c81b48674d24e6c5eb10845e
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-15 17:04:08 -0500

    add config vite test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'html'], 
+      all: true,
+      include: ['src/api/**', 'src/pages/Home/**'], 
+    },
+  },
+});
