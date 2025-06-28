448a97807c0842397ee703eeabdebbf95bf9d83a
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-06-28 16:16:11 -0500

    update router order files in project

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,33 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useAuthStore } from '../context/useAuthStore'
-import { AdminLayout } from '../layouts/AdminLayout'
-import { AdminDashboard } from '../pages/AdminDashboard'
-import { ClientLayout } from '../layouts/ClientLayout'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthStore } from '../context/useAuthStore';
+import ClientLayout from '../layouts/ClientLayout';
+import AdminLayout from '../layouts/AdminLayout';
+import Login from '../screens/Auth/Login';
+import Register from '../screens/Auth/Register';
+import Home from '../screens/Home/Home';
+import ProductListPage from '../screens/Products/ProductListPage';
+import ProductDetailPage from '../screens/Products/ProductDetailPage';
+import OrdersPage from '../screens/Orders/OrdersPage';
+import OrderDetailPage from '../screens/Orders/OrderDetailPage';
+import CreateOrderPage from '../screens/Orders/CreateOrderPage';
+import AdminDashboard from '../screens/Admin/AdminDashboard';
+import ProfilePage from '../screens/profile/ProfilePage';
 
-import Login from '../pages/Login/Login'
-import Register from '../pages/Register/Register'
-import Home from '../pages/Home/Home'
 
 const AppRouter = () => {
-  const { user } = useAuthStore()
-
-  let protectedRoutes
-
-  if (user?.role === 'admin') {
-    protectedRoutes = (
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<AdminDashboard />} />
-      </Route>
-    )
-  } else if (user) {
-    protectedRoutes = (
-      <Route path="/" element={<ClientLayout />}>
-        <Route index element={<Home />} />
-      </Route>
-    )
-  } else {
-    protectedRoutes = <Route path="*" element={<Navigate to="/login" replace />} />
-  }
+  const { user } = useAuthStore();
 
   return (
     <BrowserRouter>
@@ -40,11 +28,52 @@ const AppRouter = () => {
           path="/register"
           element={!user ? <Register /> : <Navigate to={user.role === 'admin' ? '/admin' : '/'} replace />}
         />
-        {protectedRoutes}
+
+        {/* Protected Routes */}
+        {user && (
+          <>
+            {user.role === 'admin' && (
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route index element={<AdminDashboard />} />
+                {/* TODO: Add more admin routes */}
+                {/* <Route path="productos" element={<AdminProductsPage />} />
+                <Route path="pedidos" element={<AdminOrdersPage />} />
+                <Route path="usuarios" element={<AdminUsersPage />} /> */}
+              </Route>
+            )}
+            {user.role === 'client' && (
+              <Route path="/" element={<ClientLayout />}>
+                <Route index element={<Home />} />
+                <Route path="productos" element={<ProductListPage />} />
+                <Route path="producto/:id" element={<ProductDetailPage />} />
+                <Route path="pedidos" element={<OrdersPage />} />
+                <Route path="pedido/:id" element={<OrderDetailPage />} />
+                <Route path="pedido" element={<CreateOrderPage />} />
+
+                <Route path="perfil" element={<ProfilePage />} />
+              </Route>
+            )}
+          </>
+        )}
+
+        <Route 
+          path="*" 
+          element={
+            <Navigate 
+              to={
+                user 
+                  ? user.role === 'admin' 
+                    ? '/admin' 
+                    : '/' 
+                  : '/login'
+              } 
+              replace 
+            />
+          } 
+        />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
-
+export default AppRouter;
