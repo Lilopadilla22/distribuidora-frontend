c0ba47142f6df2430f5e25538e1a164d8a2c7a76
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-07-12 20:37:31 -0500

    update url backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import type { Category, Product, RegisterFormData, CreateOrderRequest, Order, OrderProductItem, UserProfile } from '../types';
 
-const API_BASE_URL = 'http://localhost:3100/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+//const API_BASE_URL = 'http://localhost:3100/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -83,6 +83,7 @@ export const registerRequest = async (data: RegisterFormData) => {
 export const fetchUserProfile = async (): Promise<UserProfile> => {
   try {
     const response = await api.get('/profile');
+    console.log(response, 'profile');
     return response.data;
   } catch (error) {
     console.error('Error fetching user profile:', error);
